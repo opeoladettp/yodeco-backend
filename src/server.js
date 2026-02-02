00c26cdf4b5371a065189023dbda3623b9d4b66d
Opeyemi Oladejobi <opeoladettp@gmail.com>
2026-02-02 16:05:50 +0100

    Use wildcard for CORS allowed headers to fix x-request-id header issue

--- src/server.js
+++ src/server.js
@@ -32,17 +32,7 @@ if (process.env.NODE_ENV === 'production' && process.env.RAILWAY_ENVIRONMENT) {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With', 
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Cache-Control',
-      'X-Admin-IP',
-      'X-Request-ID',
-      'x-request-id'
-    ],
+    allowedHeaders: '*',
     exposedHeaders: ['set-cookie'],
     optionsSuccessStatus: 200
   };
