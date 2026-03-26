54b366c191f350eb058751b8bdfca17fc49de9e1
Opeyemi Oladejobi <opeoladettp@gmail.com>
2026-03-26 19:39:03 +0100

    Fix SyntaxError: simplify imgTag onerror to avoid nested quote issue in generated JS

--- src/routes/presentation.js
+++ src/routes/presentation.js
@@ -305,7 +305,7 @@ function esc(s) {
 
 function imgTag(localImage, alt, cls) {
   if (!localImage) return '<div class="' + cls + '">&#128100;</div>';
-  return '<img src="' + localImage + '" alt="' + esc(alt) + '" onerror="this.parentElement.innerHTML=\'<div class=\\\\\'' + cls + '\\\\\'> &#128100;</div>\'">';
+  return '<img src="' + localImage + '" alt="' + esc(alt) + '" onerror="this.style.display=\'none\'">';
 }
 
 function buildSlide(s) {
